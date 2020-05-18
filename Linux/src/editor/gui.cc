3c3e20fbced766d153019bf639ce57a7b3384778
BitTim <tim.anhalt.2004@protonmail.com>
2020-05-18 13:19:24 +0200

    Removed redundant presentCreator and improved Error feedback

--- Linux/src/editor/gui.cc
+++ Linux/src/editor/gui.cc
@@ -87,6 +87,7 @@ EditorGUIMain::EditorGUIMain() : wxFrame(nullptr, wxID_ANY, "")
   wxFont font = textEdit->GetFont();
   font.SetFamily(wxFONTFAMILY_TELETYPE);
   textEdit->SetFont(font);
+  textEdit->SetBackgroundColour(*wxWHITE);
 
   font = warnings->GetFont();
   font.SetFamily(wxFONTFAMILY_TELETYPE);
@@ -132,34 +133,55 @@ void EditorGUIMain::update()
     Global::_PREVIEWTHREAD = std::thread(&EditorGUIMain::previewUpdate, this);
     Global::_PREVIEWTHREAD.detach();
 
-    warnings->Clear();
-
-    for(int i = 0; i < Global::_ERRORS.size(); i++)
-    {
-      if(Global::_ERRORS[i].type == ERROR_ERROR) warnings->SetDefaultStyle(wxTextAttr(wxColor(0xC0, 0x3B, 0x3A)));
-      if(Global::_ERRORS[i].type == ERROR_WARNING) warnings->SetDefaultStyle(wxTextAttr(wxColor(0xF5, 0xD5, 0x57)));
-
-      warnings->AppendText("Line " + std::to_string(Global::_ERRORS[i].line) + ": " + Global::_ERRORS[i].desc + "\n");
-    }
-
-    if(Global::_ERRORS.size() == 0)
-    {
-      warnings->SetDefaultStyle(wxTextAttr(wxColor(0x89, 0xB1, 0x7E)));
-      warnings->AppendText("No warnings or errors");
-    }
-
+    //Syntax Highlighting
     for(int i = 0; i < tokens.size(); i++)
     {
       std::string buffer = std::string(textEdit->GetValue());
       int tokenStart = buffer.find(tokens[i].value);
       int tokenEnd = tokenStart + tokens[i].value.length() + 1;
 
-      if(tokens[i].value[0] == '.') textEdit->SetStyle(tokenStart, tokenEnd, wxTextAttr(wxColor(0x29, 0x3C, 0x7A)));
-      else if(tokens[i].value[0] == '*') textEdit->SetStyle(tokenStart, tokenEnd, wxTextAttr(wxColor(0x21, 0x54, 0x63)));
-      else if(tokens[i].value[0] == '<' && tokens[i].value[tokens[i].value.length() - 1] == '>') textEdit->SetStyle(tokenStart, tokenEnd, wxTextAttr(wxColor(0x43, 0x6F, 0x25)));
-      else if(tokens[i].value[0] == '[' && tokens[i].value[tokens[i].value.length() - 1] == ']') textEdit->SetStyle(tokenStart, tokenEnd, wxTextAttr(wxColor(0xA1, 0x36, 0x42)));
-      else if(tokens[i].value[0] == '-' && tokens[i].value[tokens[i].value.length() - 1] == '-') textEdit->SetStyle(tokenStart, tokenEnd, wxTextAttr(wxColor(0xC3, 0x99, 0x4B)));
-      else textEdit->SetStyle(tokenStart, tokenEnd, wxTextAttr(wxColor(0x10, 0x09, 0x1B)));
+      if(tokens[i].value[0] == '.') textEdit->SetStyle(tokenStart, tokenEnd, wxTextAttr(wxColor(0x29, 0x3C, 0x7A), *wxWHITE));
+      else if(tokens[i].value[0] == '*') textEdit->SetStyle(tokenStart, tokenEnd, wxTextAttr(wxColor(0x21, 0x54, 0x63), *wxWHITE));
+      else if(tokens[i].value[0] == '<' && tokens[i].value[tokens[i].value.length() - 1] == '>') textEdit->SetStyle(tokenStart, tokenEnd, wxTextAttr(wxColor(0x43, 0x6F, 0x25), *wxWHITE));
+      else if(tokens[i].value[0] == '[' && tokens[i].value[tokens[i].value.length() - 1] == ']') textEdit->SetStyle(tokenStart, tokenEnd, wxTextAttr(wxColor(0xA1, 0x36, 0x42), *wxWHITE));
+      else if(tokens[i].value[0] == '-' && tokens[i].value[tokens[i].value.length() - 1] == '-') textEdit->SetStyle(tokenStart, tokenEnd, wxTextAttr(wxColor(0xC3, 0x99, 0x4B), *wxWHITE));
+      else textEdit->SetStyle(tokenStart, tokenEnd, wxTextAttr(wxColor(0x10, 0x09, 0x1B), *wxWHITE));
+    }
+
+    //Warnings and Errors
+    warnings->Clear();
+
+    for(int i = 0; i < Global::_ERRORS.size(); i++)
+    {
+      long cursorStart;
+      long cursorEnd;
+
+      if(Global::_ERRORS[i].line != -1)
+      {
+       cursorStart = textEdit->XYToPosition(0, Global::_ERRORS[i].line - 1);
+       cursorEnd = cursorStart + textEdit->GetLineLength(Global::_ERRORS[i].line - 1);
+      }
+
+      if(Global::_ERRORS[i].type == ERROR_ERROR)
+      {
+        warnings->SetDefaultStyle(wxTextAttr(wxColor(0xC0, 0x3B, 0x3A)));
+        if(Global::_ERRORS[i].line != -1) textEdit->SetStyle(cursorStart, cursorEnd, wxTextAttr(wxNullColour, wxColor(0xDE, 0x9C, 0x9C)));
+      }
+
+      if(Global::_ERRORS[i].type == ERROR_WARNING)
+      {
+        warnings->SetDefaultStyle(wxTextAttr(wxColor(0xF5, 0xD5, 0x57)));
+        if(Global::_ERRORS[i].line != -1) textEdit->SetStyle(cursorStart, cursorEnd, wxTextAttr(wxNullColour, wxColor(0xDD, 0xC7, 0x98)));
+      }
+
+      if(Global::_ERRORS[i].line != -1) warnings->AppendText("Line " + std::to_string(Global::_ERRORS[i].line) + ": ");
+      warnings->AppendText(Global::_ERRORS[i].desc + "\n");
+    }
+
+    if(Global::_ERRORS.size() == 0)
+    {
+      warnings->SetDefaultStyle(wxTextAttr(wxColor(0x89, 0xB1, 0x7E)));
+      warnings->AppendText("No warnings or errors");
     }
   }
 }
@@ -231,6 +253,7 @@ bool EditorGUIApp::OnInit()
 	Global::_PRESENT->font = Global::_DEFAULTFONT;
 
   mainFrame->prevCursorPos = mainFrame->textEdit->GetInsertionPoint();
+  mainFrame->SetBackgroundColour(*wxWHITE);
   mainFrame->cursor->Start(1);
 
   return true;
