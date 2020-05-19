56fa861f14a7f42deb42c8538ec23d2fcf4f1b3b
BitTim <tim.anhalt.2004@protonmail.com>
2020-05-19 13:41:12 +0200

    Removed Compiler

--- Linux/src/compiler/lib/fileParser.hh
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-extern int nDotWarn;
-
-extern std::vector<std::string> tokenize(std::string path);
-extern void parse(std::vector<std::string> tokens);
--- Linux/src/compiler/lib/pdfCreator.hh
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "hpdf.h"
-#include <string>
-
-extern void errorHandler(HPDF_STATUS error_no, HPDF_STATUS detail_no, void *user_data);
-extern int createPDF(std::string outpath);
